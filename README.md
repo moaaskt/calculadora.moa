af41c59027257281160bf6732de1d7b0dc0f3317
moaaskt <moacirmayer@hotmail.com>
2022-03-25 16:52:42 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-aedrkv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-aedrkv)
---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { disableDebugTools } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  n1;
+  n2;
+  resultado;
+  operation(op) {
+    if(this.n1 == undefined || this.n2 == undefined){
+      alert("Digite um numero")
+      return 
+    }
+    switch (op) {
+      case '+':
+        this.resultado = this.n1 + this.n2;
+        break;
+      case '-':
+        this.resultado = this.n1 - this.n2;
+        break;
+      case '/':
+        this.resultado = this.n1 / this.n2;
+        break;
+      case '*':
+        this.resultado = this.n1 * this.n2;
+        break;
+    
+    }
+  }
+Limpar(){
+    this.n1 = disableDebugTools,
+    this.n2 = disableDebugTools;
+    this.resultado = undefined;
+  }
+
+}
